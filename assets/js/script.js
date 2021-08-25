44a8ae273113c33c26ea4fc808b9d5f59e09c478
rafaelcastrobr <rafaelcastrobr@gmail.com>
2021-08-25 20:06:30 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,30 @@
 
-
+// ==== botão que gera resultado
 const btnGera = document.querySelector('.gera-resultado__btn');
 btnGera.addEventListener('click', () => {
   const textArea = document.querySelector('.resultado-area')
   let titulo = document.querySelectorAll('.lista-dados--titulo');
   let diretor = document.querySelectorAll('.lista-dados--diretor');
+  let id = document.querySelectorAll('.lista-imdb--linkImdb');
+  let fest = document.querySelector('.lista-festival--titulo').value;
   
   textArea.innerHTML = ``;
   for(let i=0; i<titulo.length; i++){
-      textArea.innerHTML += (`${titulo[i].value}, de ${diretor[i].value}\n`);
+      textArea.innerHTML = `[center][size=6][b][color=#000080]SELEÇÃO OFICIAL DE ${fest.toUpperCase()} [/color][/b][/size][/center]\n`
+      textArea.innerHTML += (`[url=https://www.imdb.com/title/${id[i].value}/][img=https://imagizer.imageshack.us/a/img923/8099/6faAjm.jpg][/url] ${titulo[i].value}, de ${diretor[i].value}\n`);
   }
 })
-  let titulos = [];
-  function imdb__btn(){
-    let titulo = document.querySelectorAll('.lista-dados--titulo');
-    for(let i=0; i<titulo.length; i++) {
-      titulos.push(titulo[i].value);
-    }
 
-    for(let j in titulos) {
-      if(titulos[j] === titulos[j]){
-          window.open(`http://www.imdb.com/find?q=${titulos[j]}`, '_blank')
-    
-      }
+// ==== gera numero aleatório para a id
+function getRandom(max=1, min=10000) {
+  return Math.floor(Math.random() * (max - min)+min)
+}
 
-    }
-      
-  }
-/*
-  const btnBusca = document.querySelectorAll('.lista-imdb__btn');
-  btnBusca.addEventListener('click', () => {
-    let titulo = document.querySelectorAll('.lista-dados--titulo');
-    console.log('oi')
-    window.open(`http://www.imdb.com/find?q=${titulos}`, '_blank')
-    //console.log(titulos)
-  });
-*/
-
+// ==== botão que adiciona novas áreas
 const btnMais = document.querySelector('.lista-mais__btn');
 btnMais.addEventListener('click', () => {
+  const numAlea = getRandom();
+
   // ===== titulo
   const proxLinha = document.querySelector('.proxLinha');
   const div = document.createElement('div');
@@ -47,8 +33,9 @@ btnMais.addEventListener('click', () => {
   label.innerHTML = 'Título:';
   div.append(label);
   const input = document.createElement('input');
-  input.className = 'lista-dados--titulo';
+  input.className = `lista-dados--titulo`;
   input.type = 'text';
+  input.id = `${numAlea}`;
   div.append(input);
   // ===== diretor
   const labelD = document.createElement('label');
@@ -62,18 +49,31 @@ btnMais.addEventListener('click', () => {
   const divIMDb = document.createElement('div');
   divIMDb.className = 'lista-imdb';
   const labelIMDb = document.createElement('label');
-  labelIMDb.innerHTML = 'IMDb:';
+  labelIMDb.innerHTML = 'ID IMDb:';
   divIMDb.append(labelIMDb);
   const inputIMDb = document.createElement('input');
   inputIMDb.className = 'lista-imdb--linkImdb';
   inputIMDb.type = 'text';
   divIMDb.append(inputIMDb)
   const inputIMDbButton = document.createElement('button');
-  inputIMDbButton.className = 'lista-imdb__btn';
-  inputIMDbButton.onclick = imdb__btn;
+  inputIMDbButton.className = `lista-imdb__btn`;
+  inputIMDbButton.id = `${numAlea}`;
   inputIMDbButton.innerHTML = 'buscar filme no imdb';
+  inputIMDbButton.onclick = btn_imdb;
   divIMDb.append(inputIMDbButton);
 
   proxLinha.append(div);
   proxLinha.appendChild(divIMDb);
 })
+
+// ==== botão que busca no imdb
+function btn_imdb() {
+
+  let numId = event.target.id;
+
+  const nomeFilm = document.getElementById(`${numId}`).value;
+
+  window.open(`http://www.imdb.com/find?q=${nomeFilm}`, '_blank')
+
+}
+
